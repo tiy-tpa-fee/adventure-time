4e185bace63be96df8e55aabdfef9e68d39ec7d8
Brian Bycynski <high5bri@hotmail.com>
2016-12-07 10:05:39 -0500

    Set Current Character

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -12,10 +12,20 @@ class CharacterList extends Component {
     }
   }
 
+  setCurrentCharacter (character) {
+    this.setState({
+      currentCharacter: character
+    })
+  }
+
   render () {
     const items = characters.map((character, i) => {
       const active = i === this.state.currentCharacter
-      return <CharacterListItem name={character.name} isActive={active} key={i} />
+      return <CharacterListItem
+        name={character.name}
+        isActive={active}
+        onSelectCharacter={() => this.setCurrentCharacter(i)}
+        key={i} />
     })
     return <ul className={styles.root}>
       {items}
--- src/components/CharacterListItem.js
+++ src/components/CharacterListItem.js
@@ -4,12 +4,15 @@ import styles from './CharacterListItem.scss'
 class CharacterListItem extends Component {
   static propTypes = {
     name: React.PropTypes.string.isRequired,
-    isActive: React.PropTypes.bool
+    isActive: React.PropTypes.bool,
+    onSelectCharacter: React.PropTypes.func
   }
 
   render () {
     const active = this.props.isActive ? styles.active : null
-    return <li className={`${styles.root} ${active}`}>
+    return <li
+      className={`${styles.root} ${active}`}
+      onClick={this.props.onSelectCharacter}>
       {this.props.name}
     </li>
   }
